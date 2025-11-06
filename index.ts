ff00f8faae87bd727a129ebf9077d4044203ebc5
Aurumeta <maurulius1@gmail.com>
2025-11-06 04:49:37 +0100

    Refactor Resend initialization and add data object
    Removed Resend initialization and added a JSON object.

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import { Resend } from 'resend';
-const resend = new Resend('re_123456789');
-
+{
+  "object": "list",
+  "has_more": false,
+  "data": [
+    {
+      "id": "91f3200a-df72-4654-b0cd-f202395f5354",
+      "name": "Production",
+      "created_at": "2023-04-08T00:11:13.110779+00:00"
+    }
+  ]
+}
 (async function() {
   try {
     const data = await resend.emails.send({
